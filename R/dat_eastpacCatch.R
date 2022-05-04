b2fad18eea29700cb0482eeef2d0c2e05a50ed2e
habeebollah <habeebollah@yahoo.com>
2022-05-04 10:20:14 -0700

    data commit

---
+++ R/dat_eastpacCatch.R
@@ -0,0 +1,14 @@
+#' @title pelagic longine fishery data
+#'
+#' @description A data set containing year, catch and effort from longline to catch pelagic fish in eastern pacific sea (Schaefer, 1957).
+#'
+#' @format A data frame with 21 rows and 3 variables:
+#' \describe{
+#'   \item{year}{year when fishing activity occured.}
+#'   \item{catch}{catch per annum in MT.}
+#'   \item{effort}{effort unit per annum.}
+#' }
+#'
+#' @references Schaefer, Milner Baily. "A study of the dynamics of the fishery for yellow fin tuna in the eastern tropical Pacific Ocean." Bull. Inter-Amer. Trop. Tuna Comm. 2 (1957): 247-285.
+#'
+"eastpacCatch"
---
+++ R/dat_goodcontrast0.R
@@ -0,0 +1,15 @@
+#' @title Good contrast data without process and catch error
+#'
+#' @description A data set containing year, catch and effort which shows CPUE in good contrast.
+#' This data shows doesn't show stochasticity using lognormal process and catch error.
+#'
+#' @format A data frame with 20 rows and 3 variables:
+#' \describe{
+#'   \item{year}{year when fishing activity occured.}
+#'   \item{catch}{catch per annum in MT.}
+#'   \item{effort}{effort unit per annum.}
+#' }
+#'
+#' @references Magnusson, Arni, and Ray Hilborn. "What makes fisheries data informative?." Fish and Fisheries 8, no. 4 (2007): 337-358.
+#'
+"goodcontrast0"
