d3576c14b2819f4781e9cfba6271be6a80b80c57
ahabibi <habeebollah@yahoo.com>
2022-05-23 16:54:06 -0700

    fixing the weighting test to follow Ray input, using value ranging 0-1

--- R/OWTfx.R
+++ R/OWTfx.R
@@ -43,74 +43,105 @@ owt.base
 # conduct parameter estimation
 pars <- c(log(K), log(B0), log(r), log(q), log(0.1))
 
-fit.owt.T250 <- fit.owt.T200 <- fit.owt.T300 <- fit.owt.T350 <- fit.owt.F <- list()
+fit.owt.T01 <- fit.owt.T02 <- fit.owt.T03 <- fit.owt.T04 <- fit.owt.T05 <- fit.owt.T06 <-
+  fit.owt.T07 <- fit.owt.T08 <- fit.owt.T09 <- fit.owt.F <- list()
 for(i in 1:nsims){
   fit.owt.F[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
                              method="Nelder-Mead", OWT=FALSE, Frate = 0.7, weight = 1000))
-  fit.owt.T250[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
-                                method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 250))
-  fit.owt.T200[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
-                                method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 200))
-  fit.owt.T300[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
-                                method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 300))
-  fit.owt.T350[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
-                                method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 350))
+  fit.owt.T01[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                                method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.1))
+  fit.owt.T02[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                                method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.2))
+  fit.owt.T03[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                               method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.3))
+  fit.owt.T04[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                               method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.4))
+  fit.owt.T05[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                               method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.5))
+  fit.owt.T06[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                               method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.6))
+  fit.owt.T07[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                               method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.7))
+  fit.owt.T08[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                               method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.8))
+  fit.owt.T09[i] <- list(optim(par=pars, fn=Spar_min, df=owt.base[[i]],
+                               method="Nelder-Mead", OWT=TRUE, Frate = 0.7, weight = 0.9))
 }
 
-pars.owt.T250 <- pars.owt.T200 <- pars.owt.T300 <- pars.owt.T350 <- pars.owt.F <- matrix(NA, nrow=nsims, ncol=5)
-colnames(pars.owt.T250) <- colnames(pars.owt.T200) <- colnames(pars.owt.T300) <-
-  colnames(pars.owt.T350) <-colnames(pars.owt.F) <- c("K", "B0", "r", "q", "sigma")
+pars.owt.T01 <- pars.owt.T02 <- pars.owt.T03 <- pars.owt.T04 <- pars.owt.T05 <- pars.owt.T06 <-
+  pars.owt.T07 <- pars.owt.T08 <- pars.owt.T09 <- pars.owt.F <- matrix(NA, nrow=nsims, ncol=5)
+colnames(pars.owt.T01) <- colnames(pars.owt.T02) <- colnames(pars.owt.T03) <- colnames(pars.owt.T04) <-
+  colnames(pars.owt.T05) <- colnames(pars.owt.T06) <- colnames(pars.owt.T07) <- colnames(pars.owt.T08) <-
+  colnames(pars.owt.T09) <-colnames(pars.owt.F) <- c("K", "B0", "r", "q", "sigma")
 for(i in 1:nsims){
   pars.owt.F[i,] <- exp(fit.owt.F[[i]]$par)
-  pars.owt.T250[i,] <- exp(fit.owt.T250[[i]]$par)
-  pars.owt.T200[i,] <- exp(fit.owt.T200[[i]]$par)
-  pars.owt.T300[i,] <- exp(fit.owt.T300[[i]]$par)
-  pars.owt.T350[i,] <- exp(fit.owt.T350[[i]]$par)
-}
+  pars.owt.T01[i,] <- exp(fit.owt.T01[[i]]$par)
+  pars.owt.T02[i,] <- exp(fit.owt.T02[[i]]$par)
+  pars.owt.T03[i,] <- exp(fit.owt.T03[[i]]$par)
+  pars.owt.T04[i,] <- exp(fit.owt.T04[[i]]$par)
+  pars.owt.T05[i,] <- exp(fit.owt.T05[[i]]$par)
+  pars.owt.T06[i,] <- exp(fit.owt.T06[[i]]$par)
+  pars.owt.T07[i,] <- exp(fit.owt.T07[[i]]$par)
+  pars.owt.T08[i,] <- exp(fit.owt.T08[[i]]$par)
+  pars.owt.T09[i,] <- exp(fit.owt.T09[[i]]$par)
+  }
 
 # calculate MSY
-RP.owt.T250 <- RP.owt.T200 <- RP.owt.T300 <- RP.owt.T350 <- RP.owt.F <- matrix(NA, nrow=nsims, ncol=3)
-colnames(RP.owt.T250) <- colnames(RP.owt.T200) <- colnames(RP.owt.T300) <-
-  colnames(RP.owt.T350) <- colnames(RP.owt.F) <- c("Bmsy", "MSY", "Emsy")
+RP.owt.T01 <- RP.owt.T02 <- RP.owt.T03 <- RP.owt.T04 <- RP.owt.T05 <- RP.owt.T06 <-
+  RP.owt.T07 <- RP.owt.T08 <- RP.owt.T09 <- RP.owt.F <- matrix(NA, nrow=nsims, ncol=3)
+colnames(RP.owt.T01) <- colnames(RP.owt.T02) <- colnames(RP.owt.T03) <- colnames(RP.owt.T04) <- colnames(RP.owt.T05) <-
+  colnames(RP.owt.T06) <- colnames(RP.owt.T07) <- colnames(RP.owt.T08) <- colnames(RP.owt.T09) <- colnames(RP.owt.F) <- c("Bmsy", "MSY", "Emsy")
 for(i in 1:nsims){
   RP.owt.F[i,] <- unlist(SFcalcRP(inpars=pars.owt.F[i,], SPmodel=1))
-  RP.owt.T250[i,] <- unlist(SFcalcRP(inpars=pars.owt.T250[i,], SPmodel=1))
-  RP.owt.T200[i,] <- unlist(SFcalcRP(inpars=pars.owt.T200[i,], SPmodel=1))
-  RP.owt.T300[i,] <- unlist(SFcalcRP(inpars=pars.owt.T300[i,], SPmodel=1))
-  RP.owt.T350[i,] <- unlist(SFcalcRP(inpars=pars.owt.T350[i,], SPmodel=1))
+  RP.owt.T01[i,] <- unlist(SFcalcRP(inpars=pars.owt.T01[i,], SPmodel=1))
+  RP.owt.T02[i,] <- unlist(SFcalcRP(inpars=pars.owt.T02[i,], SPmodel=1))
+  RP.owt.T03[i,] <- unlist(SFcalcRP(inpars=pars.owt.T03[i,], SPmodel=1))
+  RP.owt.T04[i,] <- unlist(SFcalcRP(inpars=pars.owt.T04[i,], SPmodel=1))
+  RP.owt.T05[i,] <- unlist(SFcalcRP(inpars=pars.owt.T05[i,], SPmodel=1))
+  RP.owt.T06[i,] <- unlist(SFcalcRP(inpars=pars.owt.T06[i,], SPmodel=1))
+  RP.owt.T07[i,] <- unlist(SFcalcRP(inpars=pars.owt.T07[i,], SPmodel=1))
+  RP.owt.T08[i,] <- unlist(SFcalcRP(inpars=pars.owt.T08[i,], SPmodel=1))
+  RP.owt.T09[i,] <- unlist(SFcalcRP(inpars=pars.owt.T09[i,], SPmodel=1))
 }
 
 plot(RP.owt.F[,1], col="red")
-points(RP.owt.T250[,1], col="blue1")
-points(RP.owt.T200[,1], col="blue2")
-points(RP.owt.T300[,1], col="blue3")
-points(RP.owt.T350[,1], col="blue4")
+points(RP.owt.T01[,1], col="blue1")
 
 # see different effect of weighting to the OWT
-# how do we know ideal weighing value, because T250 gives median r 0.2 ? but K and q doesn't give a close value like r!
-Sparbase <- data.frame(OWT=rep(c("F", "T250", "T200", "T300", "T350"), each=nsims),
-                       K=c(pars.owt.F[,1], pars.owt.T250[,1], pars.owt.T200[,1], pars.owt.T300[,1], pars.owt.T350[,1]),
-                       B0=c(pars.owt.F[,2], pars.owt.T250[,2], pars.owt.T200[,2], pars.owt.T300[,2], pars.owt.T350[,2]),
-                       r=c(pars.owt.F[,3], pars.owt.T250[,3], pars.owt.T200[,3], pars.owt.T300[,3], pars.owt.T350[,3]),
-                       q=c(pars.owt.F[,4], pars.owt.T250[,4], pars.owt.T200[,4], pars.owt.T300[,4], pars.owt.T350[,4]),
-                       sigma=c(pars.owt.F[,5], pars.owt.T250[,5], pars.owt.T200[,5], pars.owt.T300[,5], pars.owt.T350[,5]))
+# higher weight, we are effectively constraining the estimation procedure to fit the auxiliary information exactly.
+# lower weight, assuming that an auxiliary observation was measured with low accuracy (high variance)
+Sparbase <- data.frame(OWT=rep(c("F", "T01", "T02", "T03", "T04", "T05", "T06", "T07", "T08", "T09"), each=nsims),
+                       K=c(pars.owt.F[,1], pars.owt.T01[,1], pars.owt.T02[,1], pars.owt.T03[,1], pars.owt.T04[,1],
+                           pars.owt.T05[,1], pars.owt.T06[,1], pars.owt.T07[,1], pars.owt.T08[,1], pars.owt.T09[,1]),
+                       B0=c(pars.owt.F[,2], pars.owt.T01[,2], pars.owt.T02[,2], pars.owt.T03[,2], pars.owt.T04[,2],
+                            pars.owt.T05[,2], pars.owt.T06[,2], pars.owt.T07[,2], pars.owt.T08[,2], pars.owt.T09[,2]),
+                       r=c(pars.owt.F[,3], pars.owt.T01[,3], pars.owt.T02[,3], pars.owt.T03[,3], pars.owt.T04[,3],
+                           pars.owt.T05[,3], pars.owt.T06[,3], pars.owt.T07[,3], pars.owt.T08[,3], pars.owt.T09[,3]),
+                       q=c(pars.owt.F[,4], pars.owt.T01[,4], pars.owt.T02[,4], pars.owt.T03[,4], pars.owt.T04[,4],
+                           pars.owt.T05[,4], pars.owt.T06[,4], pars.owt.T07[,4], pars.owt.T08[,4], pars.owt.T09[,4]),
+                       sigma=c(pars.owt.F[,5], pars.owt.T01[,5], pars.owt.T02[,5], pars.owt.T03[,5], pars.owt.T04[,5],
+                               pars.owt.T05[,5], pars.owt.T06[,5], pars.owt.T07[,5], pars.owt.T08[,5], pars.owt.T09[,5]))
+
 
 par(mfrow=c(1, 5))
-boxplot(Sparbase[,2]~Sparbase[,1], xlab="Weighting", ylab="K", ylim=c(0,3000))
-boxplot(Sparbase[,3]~Sparbase[,1], xlab="Weighting", ylab="B0", ylim=c(0,3000))
-boxplot(Sparbase[,4]~Sparbase[,1], xlab="Weighting", ylab="r", ylim=c(0,3))
+boxplot(Sparbase[,2]~Sparbase[,1], xlab="Weighting", ylab="K", ylim=c(0,2000))
+boxplot(Sparbase[,3]~Sparbase[,1], xlab="Weighting", ylab="B0", ylim=c(0,2000))
+boxplot(Sparbase[,4]~Sparbase[,1], xlab="Weighting", ylab="r", ylim=c(0,1))
 boxplot(Sparbase[,5]~Sparbase[,1], xlab="Weighting", ylab="q")
 boxplot(Sparbase[,6]~Sparbase[,1], xlab="Weighting", ylab="sigma")
 
-RPbase <- data.frame(OWT=rep(c("F", "T250", "T200", "T300", "T350"), each=nsims),
-                     Bmsy=c(RP.owt.F[,1], RP.owt.T250[,1], RP.owt.T200[,1], RP.owt.T300[,1], RP.owt.T350[,1]),
-                     MSY=c(RP.owt.F[,2], RP.owt.T250[,2], RP.owt.T200[,2], RP.owt.T300[,2], RP.owt.T350[,2]),
-                     Emsy=c(RP.owt.F[,3], RP.owt.T250[,3], RP.owt.T200[,3], RP.owt.T300[,3], RP.owt.T350[,3]))
+RPbase <- data.frame(OWT=rep(c("F", "T01", "T02", "T03", "T04", "T05", "T06", "T07", "T08", "T09"), each=nsims),
+                     Bmsy=c(RP.owt.F[,1], RP.owt.T01[,1], RP.owt.T02[,1], RP.owt.T03[,1], RP.owt.T04[,1],
+                            RP.owt.T05[,1], RP.owt.T06[,1], RP.owt.T07[,1], RP.owt.T08[,1], RP.owt.T09[,1]),
+                     MSY=c(RP.owt.F[,2], RP.owt.T01[,2], RP.owt.T02[,2], RP.owt.T03[,2], RP.owt.T04[,2],
+                           RP.owt.T05[,2], RP.owt.T06[,2], RP.owt.T07[,2], RP.owt.T08[,2], RP.owt.T09[,2]),
+                     Emsy=c(RP.owt.F[,3], RP.owt.T01[,3], RP.owt.T02[,3], RP.owt.T03[,3], RP.owt.T04[,3],
+                            RP.owt.T05[,3], RP.owt.T06[,3], RP.owt.T07[,3], RP.owt.T08[,3], RP.owt.T09[,3]))
 
 par(mfrow=c(1, 3))
-boxplot(RPbase[,2]~RPbase[,1], xlab="Weighting", ylab="Bmsy", ylim=c(0, 2000))
-boxplot(RPbase[,3]~RPbase[,1], xlab="Weighting", ylab="MSY", ylim=c(0, 200))
-boxplot(RPbase[,4]~RPbase[,1], xlab="Weighting", ylab="Emsy")
+boxplot(RPbase[,2]~RPbase[,1], xlab="Weighting", ylab="Bmsy", ylim=c(0, 1000))
+boxplot(RPbase[,3]~RPbase[,1], xlab="Weighting", ylab="MSY", ylim=c(0, 100))
+boxplot(RPbase[,4]~RPbase[,1], xlab="Weighting", ylab="Emsy", ylim=c(0, 1000))
 
 # create projection
 msyproj.gc.base <- emsyproj.gc.base <- list()
